a2799a9bdb481645a30db3172d87f7cdb0b801b2
Fizzixx4 <gregory2koch@yahoo.fr>
2022-12-12 22:22:41 +0100

    valid and delete task

--- tasks/src/components/App.js
+++ tasks/src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   // Déclare une nouvelle variable d'état, que l'on va appeler « count »
   // useState renvoie un tableau. Le premier élément de ce dernier est un état et le deuxième élément est une référence vers la fonction qui permet de modifier cet état.
   const [tasks, setTasks] = useState([
-    { title: "Faire le ménage", id: 1, is_validate: false },
+    { title: "Faire le ménage", id: 1, is_validate: false},
   ]);
 
   // Equivalent du componentDidMount si le deuxième paramètre de useEffect est []
@@ -14,20 +14,38 @@ function App() {
     // Ajout d'une tâche dans le state "tasks"
     setTasks([
       ...tasks,
-      { title: "Faire du sport", id: 2, is_validate: false },
+      { title: "Faire du sport", id: 2, is_validate: false},
     ]);
   }, []);
 
   const handleClickValidateTask = (index) =>{
-    console.log('hi');
-    setTasks(tasks[index].is_validate = true)
+    setTasks(
+      tasks.map((task, i) => {
+        if(i == index){
+          task.is_validate = !task.is_validate;
+        }
+        return task;
+      })
+    )
+  };
+
+  const handleClickDeleteTask = (index) =>{
+    setTasks(
+      tasks.filter((task, i) => i != index)
+    )
   }
 
   return (
     <div className="App container">
       <h1>Liste des tâches</h1>
       {tasks.map((task, index) => (
-        <Task task={task} key={task.id} index={index} handleClickValidateTask={handleClickValidateTask}/>
+        <Task 
+        task={task} 
+        key={task.id} 
+        handleClickValidateTask={handleClickValidateTask}
+        handleClickDeleteTask={handleClickDeleteTask}
+        index={index} 
+        />
       ))}
     </div>
   );
--- tasks/src/components/Task.js
+++ tasks/src/components/Task.js
@@ -1,16 +1,25 @@
 const Task = (props) => {
     return (
       <section className="d-flex justify-content-between">
-        <h2>{props.task.title}</h2>
+        <h2 
+        className={props.task.is_validate ? "text-decoration-line-through" : ""}
+        >
+        {props.task.title}</h2>
         <div>
-            <button className="btn btn-success me-3"
-            onClick={
-                (event)=>{
-                    props.handleClickValidateTask();
-                }
+          <button className="btn btn-success me-3"
+          onClick={
+            (event)=>{
+                props.handleClickValidateTask(props.index);
             }
-            >Valider</button>
-          <button className="btn btn-danger">Supprimer</button>
+          }
+          >Valider</button>
+          <button className="btn btn-danger"
+          onClick={
+            (event)=>{
+                props.handleClickDeleteTask(props.index);
+            }
+         }
+          >Supprimer</button>
         </div>
       </section>
     );
